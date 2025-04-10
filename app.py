74b4d98f1235d9edd1e0e198408b92d830286ac4
JBC2024 <jbarcoc@gmail.com>
2025-04-10 18:46:17 +0200

    First endpoint

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, jsonify, request
+import flask
 import os
+
+import endpoint_home
 
 # os.chdir(os.path.dirname(__file__))
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
+app = flask.Flask(__name__)
+# app.config['DEBUG'] = True
 
 # Enruta la landing page (endpoint /)
+@app.route('/', methods=['GET'])
+def home():
+    return endpoint_home.function()
 
 # Enruta la funcion al endpoint /api/v1/predict
 
 # Enruta la funcion al endpoint /api/v1/retrain
 
-# Ponerlo así para que sirva a la hora de desplegarlo en servidores
-if __name__ == "__mmain__"
-    app.run(debug=True)
+# Enruta la funcion al endpoint /api/v1/opcional
+
+debug = False
+if __name__ == "__mmain__":
+    debug=True
+
+app.run(debug)
---
+++ endpoint_home.py
@@ -0,0 +1,5 @@
+def function():
+    return """
+    <h1>Team Challenge Bender</h1>
+    <p><img src="https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2022/02/futurama-regreso-2612065.jpg?tf=1200x" />
+    """
