74b169c41ef37c33e0b87c4d85ac137302f9056b
mPuchadesdo <88624129+mPuchadesdo@users.noreply.github.com>
2025-04-20 17:30:29 +0200

    update

--- app.py
+++ app.py
@@ -2,18 +2,22 @@ import flask
 import os
 
 import endpoint_home
+import endpoint_prediction
 
 # os.chdir(os.path.dirname(__file__))
 
 app = flask.Flask(__name__)
-app.config['DEBUG'] = True
+# app.config['DEBUG'] = True
 
 # Enruta la landing page (endpoint /)
-@app.route('/', methods=['GET'])
+@app.route('/', methods = ['GET'])
 def home():
     return endpoint_home.function()
 
 # Enruta la funcion al endpoint /api/v1/predict
+@app.route('/api/v1/predict', methods = ['GET'])
+def predict():
+    return endpoint_prediction.function()
 
 # Enruta la funcion al endpoint /api/v1/retrain
 
@@ -21,6 +25,6 @@ def home():
 
 debug = False
 if __name__ == "__mmain__":
-    debug=True
+    debug = True
 
 app.run(debug=debug, host='0.0.0.0', port=8080)
