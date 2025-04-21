cf8c44149ade331da7cff39e1b3d96e0d7f48424
antonio-herrera-gb <antonioherrera764@gmail.com>
2025-04-21 10:41:13 +0200

    redeploy

--- endpoint_home.py
+++ endpoint_home.py
@@ -10,6 +10,7 @@ def function():
         <li><b>/</b> → La página principal con las instrucciones</li>
         <li><b>/api/v1/predict</b> → Devuelve una predicción. Necesitas pasarle parámetros.</li>
         <li><b>/api/v1/retrain</b> → Reentrena el modelo con nuevos datos.</li>
+        <li><b>/api/v1/redeploy</b> → Redespliega el modelo.</li>
     </ul>
 
     <p><img src="https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2022/02/futurama-regreso-2612065.jpg?tf=1200x" /></p>
