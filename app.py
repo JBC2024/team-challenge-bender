2a3fa50b0566d2376353bcc7463ac362a481a380
antonio-herrera-gb <antonioherrera764@gmail.com>
2025-04-21 11:19:52 +0200

    app redeploy

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 import endpoint_home
 import endpoint_prediction
-
+import endpoint_redeploy
 # os.chdir(os.path.dirname(__file__))
 
 app = flask.Flask(__name__)
@@ -19,7 +19,10 @@ def home():
 def predict():
     return endpoint_prediction.function()
 
-# Enruta la funcion al endpoint /api/v1/retrain
+# Enruta la funcion al endpoint /api/v1/redeploy
+@app.route('/api/v1/redeploy', methods = ['POST'])
+def redeploy():
+    return endpoint_redeploy.function()
 
 # Enruta la funcion al endpoint /api/v1/opcional
 
