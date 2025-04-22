cae4e9ca49f8b6149a5cfc9d2dedccbc7ea33b4f
mPuchadesdo <88624129+mPuchadesdo@users.noreply.github.com>
2025-04-22 12:25:54 +0200

    Update

--- endpoint_retrain.py
+++ endpoint_retrain.py
@@ -2,23 +2,29 @@ from flask import jsonify, request
 import pandas as pd
 import os
 import joblib
+import sys
+sys.path.insert(0, './')
+import utils.common as cm
 
 def function():
     file_name = ""
-    if 'file' in request.args:
+    if 'file' in request.args and request.args['file'] != '':
         file_name = request.args['file']
 
         path = f"data/{file_name}.csv"
 
         if os.path.exists(path):
-            new_data = pd.read_csv(path)
-                
+            new_data = cm.get_dataframe(path)
+            target = 'fuel_comb_mpg'
+            y_new_data = new_data[target]
+
             # Cargamos el modelo
             modelo = joblib.load("model.joblib")
 
             # Reentrenamiento
-            modelo.fit(new_data)
+            modelo.fit(new_data, y_new_data)
             joblib.dump("model.joblib")
+            print('\nEl modelo se ha entrenado\n')
 
             return jsonify(f'El modelo ha sido reentrenado con el archivo {file_name}')
         
